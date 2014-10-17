581eb3f4a1a13bc2bac9be1ea4874ca45edadb6b
Trey Dockendorf <treydock@tamu.edu>
2014-10-16 23:15:11 -0500

    Use older version of tesseract

--- modulefiles/emop.lua
+++ modulefiles/emop.lua
@@ -3,5 +3,6 @@ load("python/2.7.8")
 load("beautifulsoup4") --local
 load("leptonica/1.71")
 load("icu/52.1")
-load("tesseract/3.03-rc1")
+--load("tesseract/3.03-rc1")
+load("tesseract/3.02-r889")
 load("java/1.7.0_67")
