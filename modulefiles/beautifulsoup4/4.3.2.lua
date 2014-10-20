ff9cc19269b9838406400b03c18105745c37bae2
Trey Dockendorf <treydock@tamu.edu>
2014-10-20 10:02:54 -0500

    Update beautifulsoup4 to install to /home/idhmc/apps

--- modulefiles/beautifulsoup4/4.3.2.lua
+++ modulefiles/beautifulsoup4/4.3.2.lua
@@ -1,13 +1,12 @@
 --[[
 
-module load gcc python
+module load gcc python/2.7.8
 
-pip install --user --verbose beautifulsoup4==4.3.2
+pip install --install-option="--prefix=/home/idhmc/apps/beautifulsoup4/4.3.2" --verbose beautifulsoup4==4.3.2
 
 ]]
 
-local home = os.getenv("HOME")
-local prefix = pathJoin(home, ".local")
+local prefix = "/home/idhmc/apps/beautifulsoup4/4.3.2"
 local pythonpath = pathJoin(prefix, "lib/python2.7/site-packages")
 
 prereq('python')
