a5b5396958a50fad9bb23c941d1b9f6d0ab164cc
Trey Dockendorf <treydock@tamu.edu>
2014-10-23 20:39:11 -0500

    emop module now sets various _HOME environment variables so the submit script doesn't have to

--- modulefiles/emop.lua
+++ modulefiles/emop.lua
@@ -6,3 +6,11 @@ load("icu/52.1")
 --load("tesseract/3.03-rc1")
 load("tesseract/3.02-r889")
 load("java/1.7.0_67")
+
+local emop_home = os.getenv("EMOP_HOME")
+
+pushenv("JUXTA_HOME", pathJoin(emop_home, "lib/juxta-cl"))
+pushenv("RETAS_HOME", pathJoin(emop_home, "lib/retas"))
+pushenv("SEASR_HOME", pathJoin(emop_home, "lib/seasr"))
+pushenv("TESSDATA_PREFIX", "/dh/data/shared/")
+pushenv("DENOISE_HOME", pathJoin(emop_home, "lib/denoise"))
