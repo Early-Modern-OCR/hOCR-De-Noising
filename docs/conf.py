ef2ff9da54b65d052392ac633fb97df42f987958
Trey Dockendorf <treydock@tamu.edu>
2015-01-05 11:52:20 -0600

    * Attempt to fix doc generation for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('%s/..' % os.path.dirname(__file__)))
+emop_lib = os.path.join(os.path.abspath('%s/..' % os.path.dirname(__file__)), 'emop')
+sys.path.insert(0, emop_lib)
 
 # -- General configuration ------------------------------------------------
 
