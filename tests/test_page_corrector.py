3957112e7639de01a29ddcc3af9cf90e80ea2e6e
Trey Dockendorf <treydock@tamu.edu>
2015-02-03 14:13:06 -0600

    Add options that can enable --dump and --save arguments when running PageCorrector
    * By default the values are False.  When each is set to True in the config.ini page-corrector section, those flags are enabled
    * Updated unit tests and slightly refactored PageCorrector unit tests to reduce duplicated lines of code
    * resolves #17

--- tests/test_page_corrector.py
+++ tests/test_page_corrector.py
@@ -16,6 +16,11 @@ class TestPageCorrector(TestCase):
         self.mock_rv.communicate.return_value = ["", ""]
         self.mock_rv.returncode = 0
         self.mock_popen.return_value = self.mock_rv
+        self.settings = default_settings()
+        self.settings.seasr_home = "/foo/lib/seasr"
+        self.job = mock_emop_job(self.settings)
+        self.page_corrector = PageCorrector(self.job)
+        self.page_corrector.dicts_dir = str(self.dicts_dir)
 
     def tearDown(self):
         self.popen_patcher.stop()
@@ -27,89 +32,102 @@ class TestPageCorrector(TestCase):
         self.dbconf = str(tmpdir.join("emop.properties"))
         self.dicts_dir = tmpdir.mkdir("dictionaries")
         self.test_dict = self.dicts_dir.join("en.dict")
-
-    @mock.patch("emop.lib.processes.page_corrector.os.path.isfile")
-    def test_run(self, mock_path_isfile):
-        settings = default_settings()
-        settings.seasr_home = "/foo/lib/seasr"
-        job = mock_emop_job(settings)
-        page_corrector = PageCorrector(job)
-        page_corrector.dicts_dir = str(self.dicts_dir)
-
         with open(str(self.test_dict), "w") as outfile:
             outfile.write(" ")
 
+    @mock.patch("emop.lib.processes.page_corrector.os.path.isfile")
+    def test_run(self, mock_path_isfile):
         mock_path_isfile.return_value = True
 
         expected_cmd = [
             "java", "-Xms128M", "-Xmx512M", "-jar", "/foo/lib/seasr/PageCorrector.jar",
             "--dbconf", self.dbconf, "-t", "/foo/lib/seasr/rules/transformations.json",
-            "-o", job.output_dir, "--stats",
+            "-o", self.job.output_dir, "--stats",
             "--alt", "2", "--max-transforms", "20", "--noiseCutoff", "0.5",
-            "--dict", str(self.test_dict), "--", job.xml_file
+            "--dict", str(self.test_dict), "--", self.job.xml_file
         ]
         stdout = "{\"total\":1,\"ignored\":0,\"correct\":0,\"corrected\":1,\"unchanged\":0}"
         results = mock_results_tuple()
         expected_results = results(None, None, 0)
         self.mock_rv.communicate.return_value[0] = stdout
 
-        retval = page_corrector.run()
+        retval = self.page_corrector.run()
         args, kwargs = self.mock_popen.call_args
 
         self.maxDiff = None
         self.assertTrue(mock_path_isfile.called)
         self.assertTrue(self.mock_popen.called)
         self.assertEqual(expected_cmd, args[0])
-        self.assertEqual(job.postproc_result.pp_health, stdout)
+        self.assertEqual(self.job.postproc_result.pp_health, stdout)
         self.assertTupleEqual(expected_results, retval)
 
+    def test_run_with_dump(self):
+        expected_cmd = [
+            "java", "-Xms128M", "-Xmx512M", "-jar", "/foo/lib/seasr/PageCorrector.jar",
+            "--dbconf", self.dbconf, "-t", "/foo/lib/seasr/rules/transformations.json",
+            "-o", self.job.output_dir, "--stats",
+            "--alt", "2", "--max-transforms", "20", "--noiseCutoff", "0.5",
+            "--dict", str(self.test_dict), "--dump", "--", self.job.xml_file
+        ]
+
+        self.page_corrector.dump = True
+        self.page_corrector.run()
+        args, kwargs = self.mock_popen.call_args
+
+        self.assertEqual(expected_cmd, args[0])
+
+    def test_run_with_save(self):
+        expected_cmd = [
+            "java", "-Xms128M", "-Xmx512M", "-jar", "/foo/lib/seasr/PageCorrector.jar",
+            "--dbconf", self.dbconf, "-t", "/foo/lib/seasr/rules/transformations.json",
+            "-o", self.job.output_dir, "--stats",
+            "--alt", "2", "--max-transforms", "20", "--noiseCutoff", "0.5",
+            "--dict", str(self.test_dict), "--save", "--", self.job.xml_file
+        ]
+
+        self.page_corrector.save = True
+        self.page_corrector.run()
+        args, kwargs = self.mock_popen.call_args
+
+        self.assertEqual(expected_cmd, args[0])
+
     def test_should_run_false(self):
-        settings = default_settings()
-        job = mock_emop_job(settings)
-        job.postproc_result.pp_health_exists = True
-        job.page_result.corr_ocr_text_path_exists = True
-        job.page_result.corr_ocr_xml_path_exists = True
-        page_corrector = PageCorrector(job)
+        self.job.postproc_result.pp_health_exists = True
+        self.job.page_result.corr_ocr_text_path_exists = True
+        self.job.page_result.corr_ocr_xml_path_exists = True
+        page_corrector = PageCorrector(self.job)
 
         self.assertFalse(page_corrector.should_run())
 
     def test_should_run_true_pp_health_missing(self):
-        settings = default_settings()
-        job = mock_emop_job(settings)
-        job.postproc_result.pp_health_exists = False
-        job.page_result.corr_ocr_text_path_exists = True
-        job.page_result.corr_ocr_xml_path_exists = True
-        page_corrector = PageCorrector(job)
+        self.job.postproc_result.pp_health_exists = False
+        self.job.page_result.corr_ocr_text_path_exists = True
+        self.job.page_result.corr_ocr_xml_path_exists = True
+        page_corrector = PageCorrector(self.job)
 
         self.assertTrue(page_corrector.should_run())
 
     def test_should_run_true_corr_ocr_text_path_missing(self):
-        settings = default_settings()
-        job = mock_emop_job(settings)
-        job.postproc_result.pp_health_exists = True
-        job.page_result.corr_ocr_text_path_exists = False
-        job.page_result.corr_ocr_xml_path_exists = True
-        page_corrector = PageCorrector(job)
+        self.job.postproc_result.pp_health_exists = True
+        self.job.page_result.corr_ocr_text_path_exists = False
+        self.job.page_result.corr_ocr_xml_path_exists = True
+        page_corrector = PageCorrector(self.job)
 
         self.assertTrue(page_corrector.should_run())
 
     def test_should_run_true_corr_ocr_xml_path_missing(self):
-        settings = default_settings()
-        job = mock_emop_job(settings)
-        job.postproc_result.pp_health_exists = True
-        job.page_result.corr_ocr_text_path_exists = True
-        job.page_result.corr_ocr_xml_path_exists = False
-        page_corrector = PageCorrector(job)
+        self.job.postproc_result.pp_health_exists = True
+        self.job.page_result.corr_ocr_text_path_exists = True
+        self.job.page_result.corr_ocr_xml_path_exists = False
+        page_corrector = PageCorrector(self.job)
 
         self.assertTrue(page_corrector.should_run())
 
     def test_should_run_true_all_values_missing(self):
-        settings = default_settings()
-        job = mock_emop_job(settings)
-        job.postproc_result.pp_health_exists = False
-        job.page_result.corr_ocr_text_path_exists = False
-        job.page_result.corr_ocr_xml_path_exists = False
-        page_corrector = PageCorrector(job)
+        self.job.postproc_result.pp_health_exists = False
+        self.job.page_result.corr_ocr_text_path_exists = False
+        self.job.page_result.corr_ocr_xml_path_exists = False
+        page_corrector = PageCorrector(self.job)
 
         self.assertTrue(page_corrector.should_run())
 
