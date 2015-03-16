3fc33a4e18fd3601d3cbfdf524126e472762337e
Matthew Christy <mchristy@tamu.edu>
2015-03-16 11:15:51 -0500

    splitting deNoising code into separate repo on github

--- emop/lib/models/emop_model.py
+++
@@ -1,7 +0,0 @@
-class EmopModel(object):
-
-    def __init__(self, settings):
-        self.settings = settings
-
-    def setattrs(self, dictionary):
-        raise NotImplementedError
--- modulefiles/emop-build.lua
+++
@@ -1,4 +0,0 @@
-load("gcc/4.8.2")
-load("java/1.7.0_67")
-load("maven/3.2.1")
-pushenv("_JAVA_OPTIONS", "-Xmx256m")
